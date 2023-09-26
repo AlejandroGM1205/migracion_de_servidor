aabd45bc8f076f5f2f8e0229ecdd5639901013dc
AlejandroGM1205 <alejandro.gutierrez1@soy.sena.edu.co>
2023-09-26 00:03:52 -0500

    Autenticacion con express usando JWT

--- src/app.js
+++ src/app.js
@@ -1,21 +1,61 @@
-const express = require('express');
-const app = express();
-const tasks = require('./tasks');
+const express = require("express");
+const tasks = require("./tasks");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
-const viewRouter = require('../list-view-router.js');
-const editRouter = require('../list-edit-router.js');
+const app = express();
+dotenv.config();
+
+const viewRouter = require("../list-view-router.js");
+const editRouter = require("../list-edit-router.js");
 app.use(express.json());
 
-
 //Responder con mi array [tasks] en formato JSON en mi servidor
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
-})
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
 
-app.use('/editTasks', editRouter);
-app.use('/viewTasks', viewRouter);
+const users = [
+  { id: 1, username: "user1", password: "password1" },
+  { id: 2, username: "user2", password: "password2" },
+];
 
+const JWT_SECRET = process.env.SECRET_KEY;
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET);
+
+  res.json({ token });
+});
+
+app.get("/protected", (req, res) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({ error: "Invalid Token" });
+    }
+
+    res.json({ message: "Token validated", decoded });
+  });
+});
+
+app.use("/editTasks", editRouter);
+app.use("/viewTasks", viewRouter);
 
 app.listen(3000, () => {
-    console.log(`http://localhost:${3000}`)
-})
+  console.log(`http://localhost:${3000}`);
+});
